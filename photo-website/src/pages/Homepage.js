af1fd8a4d6b4c410a0d4059babd0b52f7cafad8d
MikoZhu <huadanzhu01@gmail.com>
2023-07-14 09:01:13 +0200

    finish the photo web- search more picture function

---
+++ photo-website/src/pages/Homepage.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import Search from "../components/Search";
+import Picture from "../components/Picture";
+
+const Homepage = () => {
+  const [input, setInput] = useState("");
+  let [data, setData] = useState(null);
+  let [page, setPage] = useState(1);
+  let [currentSearch, setCurrentSearch] = useState("");
+  const auth = "9p867A0eFkKxBGh308fcQMG5sDwAi00quSdz2L88euUunUaiCFenotw5";
+  const initalURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
+  const searchURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=1`;
+  // fetech data from pexels api
+  const search = async (url) => {
+    setPage(2);
+    const dataFetch = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    });
+    let parseData = await dataFetch.json();
+    setData(parseData.photos);
+  };
+  // load more picture
+  const morepicture = async () => {
+    let newURL;
+    if (input === "") {
+      newURL = `https://api.pexels.com/v1/curated?page=${page}&per_page=15`;
+    } else {
+      newURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${page}`;
+    }
+    setPage(page + 1);
+    const dataFetch = await fetch(newURL, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    });
+    let parseData = await dataFetch.json();
+    setData(data.concat(parseData.photos));
+  };
+  // fetch data when the page loads up
+  useEffect(() => {
+    search(initalURL);
+  }, []);
+  useEffect(() => {
+    if (currentSearch === "") {
+      search(initalURL);
+    } else {
+      search(searchURL);
+    }
+  }, [currentSearch]);
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <Search
+        search={() => {
+          // JS Closure
+          setCurrentSearch(input);
+        }}
+        setInput={setInput}
+      />
+      <div className="pictures">
+        {data &&
+          data.map((d) => {
+            return <Picture data={d} />;
+          })}
+      </div>
+      <div className="morePicture">
+        <button onClick={morepicture}>Load More</button>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
